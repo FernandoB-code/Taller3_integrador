cc2609909314be24b538a0e2437486984b848bda
FernandoB-code <fernandobernasconi@gmail.com>
2022-06-12 19:06:59 -0300

    merge ayelen's branch recPass

--- app/src/main/java/com/example/login/fragments/RecoveryPasswordFragment.kt
+++ app/src/main/java/com/example/login/fragments/RecoveryPasswordFragment.kt
@@ -7,9 +7,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.findNavController
 import com.example.login.R
 import com.example.login.viewModels.RecoveryPasswordViewModel
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 
 class RecoveryPasswordFragment : Fragment() {
 
@@ -21,6 +25,9 @@ class RecoveryPasswordFragment : Fragment() {
 
     private lateinit var v: View
     private lateinit var btnRecoverPw : Button
+    private lateinit var txtEmail: EditText
+    private lateinit var rootLayout: ConstraintLayout
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,6 +35,9 @@ class RecoveryPasswordFragment : Fragment() {
     ): View? {
         v = inflater.inflate(R.layout.fragment_recovery_password, container, false)
         btnRecoverPw = v.findViewById(R.id.btnRecPassword)
+        txtEmail = v.findViewById(R.id.emailForgotPw)
+        auth = FirebaseAuth.getInstance()
+        rootLayout = v.findViewById(R.id.emailAdressRec)
         return v
     }
 
@@ -35,8 +45,35 @@ class RecoveryPasswordFragment : Fragment() {
         super.onStart()
 
         btnRecoverPw.setOnClickListener {
-            val action = RecoveryPasswordFragmentDirections.actionRecoveryPasswordFragmentToFragment1()
-            v.findNavController().navigate(action)
+            sendMail()
+        }
+    }
+
+    private fun sendMail() {
+        val email = txtEmail.text.toString()
+        if (email.isEmpty()) {
+            Snackbar.make(rootLayout, "Por favor ingresar su Email", Snackbar.LENGTH_SHORT)
+                .show()
+        } else {
+            FirebaseAuth.getInstance().sendPasswordResetEmail(email)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Snackbar.make(
+                            rootLayout,
+                            "Se envio el mail con exito",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                        v.findNavController()
+                            .navigate(RecoveryPasswordFragmentDirections.actionRecoveryPasswordFragmentToFragment1())
+
+                    } else {
+                        Snackbar.make(
+                            rootLayout,
+                            "Problemas para enviar el mail de recuperación",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                    }
+                }
         }
     }
 
