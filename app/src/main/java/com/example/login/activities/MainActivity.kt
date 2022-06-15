5c5c6a5d7e54c6c38d1974b28d9f58bb79322000
Camila Jaurena <jaurenacamila@gmail.com>
2022-06-15 11:45:59 -0300

    vistas

--- app/src/main/java/com/example/login/activities/MainActivity.kt
+++ app/src/main/java/com/example/login/activities/MainActivity.kt
@@ -6,6 +6,10 @@ import com.example.login.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        Thread.sleep(2000)
+        setTheme(R.style.Theme_Login)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
