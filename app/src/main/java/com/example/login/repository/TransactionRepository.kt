9f13aab8d81735cd052e79c542de1819f9aa48d9
Camila Jaurena <jaurenacamila@gmail.com>
2022-06-16 14:35:38 -0300

    show amount on home screen

--- app/src/main/java/com/example/login/repository/TransactionRepository.kt
+++ app/src/main/java/com/example/login/repository/TransactionRepository.kt
@@ -1,12 +1,25 @@
 package com.example.login.repository
 
+import android.content.ContentValues
 import android.icu.text.SimpleDateFormat
+import android.util.Log
+import com.example.login.entity.Account
 import com.example.login.entity.Transaction
-import java.time.LocalDate.now
-import java.time.LocalDateTime.now
+import com.example.login.entity.TransactionDetail
+import com.example.login.entity.User
+import com.example.login.util.enums.txTypeEnum
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.tasks.await
 import java.util.*
 
 class TransactionRepository {
+
+    private val db = Firebase.firestore
+    private val auth = Firebase.auth
 
     var transactionList : MutableList<Transaction> = mutableListOf()
 
@@ -27,4 +40,24 @@ class TransactionRepository {
 
     }
 
+    suspend fun getAccountFrom(): Account {
+        var account = Account()
+        val questionRef = db.collection("users")
+        try {
+            var accountOwner= auth.currentUser?.email.toString()
+            if(accountOwner != null) {
+                val data = questionRef.document(accountOwner).get().await()
+                var actualUser = data.toObject<User>()
+                if(actualUser != null) {
+                    val qRef= db.collection("accounts").document(actualUser.accountID).get().await()
+                    if(qRef.exists() && qRef != null) {
+                        account = qRef.toObject<Account>()!!
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            Log.v("Error", e.toString())
+        }
+        return account
+    }
 }
