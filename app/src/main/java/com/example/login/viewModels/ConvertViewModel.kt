7bc6a72baf6af17d0455f73e0067fc56e6321ff4
Clara-Franzoni <franzoniclara23@gmail.com>
2022-06-18 11:52:03 -0300

    Conversor1.0

--- app/src/main/java/com/example/login/viewModels/ConvertViewModel.kt
+++ app/src/main/java/com/example/login/viewModels/ConvertViewModel.kt
@@ -1,7 +1,44 @@
 package com.example.login.viewModels
 
 import androidx.lifecycle.ViewModel
+import com.example.login.entity.APIService
+import com.example.login.fragments.ConvertFragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class ConvertViewModel : ViewModel() {
+    lateinit var money : Number
+    lateinit var fragment: ConvertFragment
     // TODO: Implement the ViewModel
+    fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://v6.exchangerate-api.com/v6/461d82411c02708209a95d88pair/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+    fun searchByAmount(query: Int) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val call = getRetrofit().create(APIService::class.java)
+                .getConversorByAmount("$query/conversion_result")
+
+
+            val amount = call.body()
+
+            if (call.isSuccessful) {
+                if (amount != null) {
+                    var money = amount.conversion_result
+                } else {
+                    fragment.showMessage("A ocurrido un error")
+                }
+            }
+
+        }
+    }
+    fun sendMoney(): Number {
+        return this.money
+    }
+
 }
